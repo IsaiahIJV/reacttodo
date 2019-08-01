c94a84831913706eb693b2ae002b199612557331
IsaiahIJV <isaiah.vertus@gmail.com>
2019-08-01 15:32:33 -0400

    Adding test build

---
+++ build/precache-manifest.8f44a422f14134734883c1bf9cf7182a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a585b84e56480bdd2a190e1220aaef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b6e058f731716430fc",
+    "url": "/static/css/main.bdc3f955.chunk.css"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "28b6e058f731716430fc",
+    "url": "/static/js/main.4d9ef661.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
